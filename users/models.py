be08cfb045bb0e94572c486e6547df7c0ffbc3bc
Sam <iaiznura@gmail.com>
2023-09-14 22:34:11 +0300

    add User model

--- users/models.py
+++ users/models.py
@@ -13,8 +13,9 @@ class User(AbstractUser):
     last_name = models.CharField(max_length=100, verbose_name='фамилия', **NULLABLE)
     is_phone_verified = models.BooleanField(verbose_name='телефон подтвержден', default=False)
     auth_code = models.SmallIntegerField(verbose_name='код подтверждения', **NULLABLE)
-    self_invite = models.CharField(max_length=6, verbose_name='инвайт код', unique=True, **NULLABLE)
-    received_invite = models.ForeignKey('self', on_delete=models.CASCADE, verbose_name='полученный инвайт код', **NULLABLE)
+    self_invite = models.CharField(max_length=6, verbose_name='инвайт код', **NULLABLE)
+    # unique = True,
+    received_invite = models.ForeignKey('self', on_delete=models.DO_NOTHING, verbose_name='полученный инвайт код', **NULLABLE)
     is_invite_received = models.BooleanField(default=False, verbose_name='введен инвайт код другого пользователя')
 
     USERNAME_FIELD = 'phone'
@@ -26,4 +27,4 @@ class User(AbstractUser):
         unique_together = ('email', 'phone', 'first_name', 'last_name')
 
     def __str__(self):
-        return self.email, self.phone
+        return f'{self.email}, {self.phone}'
