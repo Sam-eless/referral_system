cd49a015fed4bdcc0dee66c6dcfaaab29fbbbfa4
Sam <iaiznura@gmail.com>
2023-09-16 01:04:19 +0300

    upd urls.py

--- config/urls.py
+++ config/urls.py
@@ -38,6 +38,6 @@ schema_view = get_schema_view(
 urlpatterns = [
                   path('admin/', admin.site.urls),
                   path('users/', include('users.urls')),
-                  path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+                  path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
                   path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
               ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
