218039c04ac609474b93fdfbcc8b269e80fe8255
Sam <iaiznura@gmail.com>
2023-09-16 13:11:23 +0300

    add shared task reset_auth_code

---
+++ users/tasks.py
@@ -0,0 +1,10 @@
+from celery import shared_task
+
+from users.models import User
+from users.services.services import reset_auth_code
+
+
+@shared_task
+def shared_reset_auth_code(user: User):
+    reset_auth_code(user)
+
