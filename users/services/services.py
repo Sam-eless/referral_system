923cadf4ab5b92bbc801071cd321f91c1619c589
Sam <iaiznura@gmail.com>
2023-09-16 13:08:17 +0300

    add func reset_auth_code to services.py

--- users/services/services.py
+++ users/services/services.py
@@ -1,6 +1,7 @@
 import string
 
 from datetime import date, timedelta, datetime
+import time
 
 from django.utils.crypto import get_random_string
 
@@ -14,3 +15,19 @@ def set_self_invite():
 
 def set_auth_code():
     return get_random_string(length=4, allowed_chars=string.digits)
+
+
+def reset_auth_code(phone):
+
+    try:
+        user = User.objects.get(phone=phone)
+        print('пользователь найден')
+        # time.sleep(60*60)
+        time.sleep(15)
+        user.is_phone_verified = False
+        user.auth_code = None
+        user.set_unusable_password()
+        user.save()
+    except Exception as error:
+        print(error)
+
